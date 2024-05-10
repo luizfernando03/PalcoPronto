bca0678fad0ea186be61f1a6528686c75748b6c5
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-10 12:42:08 -0300

    Subindo dados da classe VendasModel

--- src/main/java/br/com/javeirosavante/palcopronto/model/VendasModel.java
+++ src/main/java/br/com/javeirosavante/palcopronto/model/VendasModel.java
@@ -1,4 +1,27 @@
 package br.com.javeirosavante.palcopronto.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "venda")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VendasModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToMany
+    @JoinColumn(name = "evento_id")
+    private EventoModel eventoModel;
+    
+    private LocalDateTime dataHora;
+
 }
