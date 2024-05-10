18bfc44969dcd71c6176853d6577477583cf3567
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-10 12:36:55 -0300

    Subindo dados da classe ingressoModel

--- src/main/java/br/com/javeirosavante/palcopronto/model/EspacoModel.java
+++ src/main/java/br/com/javeirosavante/palcopronto/model/EspacoModel.java
@@ -1,4 +1,27 @@
 package br.com.javeirosavante.palcopronto.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "espaco")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EspacoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull
+    private String descricao;
+
+    @NotNull
+    private int capacidademaxima;
+
+    private boolean disponivel;
 }
--- src/main/java/br/com/javeirosavante/palcopronto/model/IngressoModel.java
+++ src/main/java/br/com/javeirosavante/palcopronto/model/IngressoModel.java
@@ -1,4 +1,27 @@
 package br.com.javeirosavante.palcopronto.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "ingresso")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class IngressoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToMany
+    @JoinColumn(name = "evento_id")
+    private EventoModel eventoModel;
+
+    private boolean vip;
+
+    private double preco;
+
 }
