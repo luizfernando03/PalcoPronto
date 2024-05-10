18dce9be787b98bb3ea73619e866c5a9cc8de07a
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-10 16:20:56 -0300

    Subindo classe VendasConfig

---
+++ src/main/java/br/com/javeirosavante/palcopronto/config/VendaConfig.java
@@ -0,0 +1,21 @@
+package br.com.javeirosavante.palcopronto.config;
+
+
+import br.com.javeirosavante.palcopronto.model.Venda;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class VendaConfig {
+
+    @Bean
+    public Venda venda() {
+        Venda venda = new Venda();
+        evento.setid();
+        evento.setnome();
+        evento.setdata();
+        evento.sethora();
+
+
+    }
+}
