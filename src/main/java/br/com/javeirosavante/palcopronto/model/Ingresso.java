2a4aa4398cc630f790c9d44603b7434260005ff3
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-13 13:40:35 -0300

    Atualizando a classe ingresso

--- src/main/java/br/com/javeirosavante/palcopronto/model/Ingresso.java
+++ src/main/java/br/com/javeirosavante/palcopronto/model/Ingresso.java
@@ -14,14 +14,18 @@ public class Ingresso {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long idIngresso;
+
+    private TipoIngresso tipoIngresso;
+
+    private Boolean ingressoDisponivel;
+
+    private Double precoIngresso;
+
+    private Long quantidadeMaxima;
 
     @ManyToMany
-    @JoinColumn(name = "eventoId")
+    @JoinColumn(name = "idEvento")
     private Evento evento;
-
-    private boolean vip;
-
-    private double preco;
 
 }
