29b6c1b8f7e4e09c66288eae522f145e38c34b16
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-13 14:30:42 -0300

    Subindo dados da classe clienteMapper

--- src/main/java/br/com/javeirosavante/palcopronto/mapper/ClienteMapper.java
+++ src/main/java/br/com/javeirosavante/palcopronto/mapper/ClienteMapper.java
@@ -1,4 +1,44 @@
 package br.com.javeirosavante.palcopronto.mapper;
 
+import br.com.javeirosavante.palcopronto.dto.ClienteDto;
+import br.com.javeirosavante.palcopronto.model.Cliente;
+
+import java.awt.*;
+import java.util.Objects;
+
 public class ClienteMapper {
+
+    public static List<ClienteDto> toDto(final List<Cliente> entities) {
+        return entities.stream()
+                .map(entity -> toDto(entity))
+                .collect(Collectors.toList());
+    }
+
+    public static ClienteDto toDto(final Cliente entity) {
+        if (Objects.nonNull(entity)) {
+            return ClienteDto.builder()
+                    .cpf(entity.getCpf())
+                    .venda(entity.getVenda())
+                    .build();
+        } else {
+            return ClienteDto.builder().build();
+        }
+    }
+
+    public static Cliente toEntity(final ClienteDto dto) {
+        if (Objects.nonNull(dto)) {
+            return Cliente.builder()
+                    .cpf(dto.getCpf())
+                    .venda(dto.getVenda())
+                    .build();
+        } else {
+            return Cliente.builder().build();
+        }
+    }
+
+    public static void copy(final Cliente source, final Cliente destiny) {
+        destiny.setCpf(source.getCpf());
+        destiny.setVenda(source.getVenda());
+    }
+
 }
--- src/main/java/br/com/javeirosavante/palcopronto/mapper/VendasMapper.java
+++ src/main/java/br/com/javeirosavante/palcopronto/mapper/VendasMapper.java
@@ -1,4 +1,7 @@
 package br.com.javeirosavante.palcopronto.mapper;
 
 public class VendasMapper {
+
+
+
 }
