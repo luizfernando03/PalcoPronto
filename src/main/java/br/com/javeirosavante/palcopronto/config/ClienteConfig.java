e3f71ff63db82360cc56e72730750883240369a0
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-13 16:27:25 -0300

    Adicionandos dados na classe IngressoMapper

--- src/main/java/br/com/javeirosavante/palcopronto/config/ClienteConfig.java
+++ src/main/java/br/com/javeirosavante/palcopronto/config/ClienteConfig.java
@@ -2,56 +2,19 @@ package br.com.javeirosavante.palcopronto.config;
 
 
 import br.com.javeirosavante.palcopronto.dto.ClienteDto;
-import br.com.javeirosavante.palcopronto.dto.EventoDto;
-import br.com.javeirosavante.palcopronto.model.Evento;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Configuration
 public class ClienteConfig {
 
-    public static List<EventoDto> toDto(final List<Evento> entities) {
-        return entities.stream()
-                .map(entity -> toDto(entity))
-                .collect(Collectors.toList());
-    }
+    @Bean
+    public List<ClienteDto> repositorioCliente() {
+        return new ArrayList<>();
 
-    public static EventoDto toDto(final Evento entity) {
-        if (Objects.nonNull(entity)) {
-            return EventoDto.builder()
-                    .idEvento(entity.getIdEvento())
-                    .nomeEvento(entity.getNomeEvento())
-                    .descricaoEvento(entity.getDescricaoEvento())
-                    .dataHoraEvento(entity.getDataHoraEvento())
-                    .build();
-        } else {
-            return EventoDto.builder().build();
-        }
-    }
-
-    public static Evento toEntity(final EventoDto dto) {
-        if (Objects.nonNull(dto)) {
-            return Evento.builder()
-                    .idEvento(dto.getIdEvento())
-                    .nomeEvento(dto.getNomeEvento())
-                    .descricaoEvento(dto.getDescricaoEvento())
-                    .dataHoraEvento(dto.getDataHoraEvento())
-                    .build();
-        } else {
-            return Evento.builder().build();
-        }
-    }
-
-    public static void copy(final Evento source, final Evento destiny) {
-        destiny.setIdEvento(source.getIdEvento());
-        destiny.setNomeEvento(source.getNomeEvento());
-        destiny.setDescricaoEvento(source.getDescricaoEvento());
-        destiny.setDataHoraEvento(source.getDataHoraEvento());
     }
 
 }
--- src/main/java/br/com/javeirosavante/palcopronto/mapper/IngressoMapper.java
+++ src/main/java/br/com/javeirosavante/palcopronto/mapper/IngressoMapper.java
@@ -1,4 +1,44 @@
 package br.com.javeirosavante.palcopronto.mapper;
 
+import br.com.javeirosavante.palcopronto.dto.IngressoDto;
+import br.com.javeirosavante.palcopronto.model.Ingresso;
+
+import java.util.List;
+import java.util.Objects;
+
 public class IngressoMapper {
+
+    public static List<IngressoDto> toDto(final List<Ingresso> entities) {
+        return entities.stream()
+                .map(entity -> toDto(entity))
+                .collect(Collectors.toList());
+    }
+
+    public static IngressoDto toDto(final Ingresso entity) {
+        if (Objects.nonNull(entity)) {
+            return IngressoDto.builder()
+                    .idIngresso(entity.getIdIngresso())
+                    .tipoIngresso(entity.getTipoIngresso())
+                    .ingressoDisponivel(entity.getIngressoDisponivel())
+                    .precoIngresso(entity.getPrecoIngresso())
+                    .quantidadeMaxima(entity.getQuantidadeMaxima())
+                    .build();
+        } else {
+            return IngressoDto.builder().build();
+        }
+    }
+
+    public static Ingresso toEntity(final IngressoDto dto) {
+        if (Objects.nonNull(dto)) {
+            return Ingresso.builder()
+                    .idIngresso(dto.getIdIngresso())
+                    .tipoIngresso(dto.getTipoIngresso())
+                    .ingressoDisponivel(dto.getIngressoDisponivel())
+                    .precoIngresso(dto.getPrecoIngresso())
+                    .quantidadeMaxima(dto.getQuantidadeMaxima())
+                    .build();
+        } else {
+            return Ingresso.builder().build();
+        }
+
 }
