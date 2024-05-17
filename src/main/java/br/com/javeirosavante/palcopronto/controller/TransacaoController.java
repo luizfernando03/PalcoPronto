b16141641f8e58246cd645c99514ae226591f6bf
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-17 16:23:46 -0300

    Adicionando dados na classe TransacaoController

--- src/main/java/br/com/javeirosavante/palcopronto/controller/TransacaoController.java
+++ src/main/java/br/com/javeirosavante/palcopronto/controller/TransacaoController.java
@@ -1,4 +1,38 @@
 package br.com.javeirosavante.palcopronto.controller;
 
+
+import br.com.javeirosavante.palcopronto.model.Transacao;
+import br.com.javeirosavante.palcopronto.service.TransacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/vendas")
 public class TransacaoController {
+
+    @Autowired
+    private TransacaoService transacaoService;
+
+    @GetMapping
+    public List<Transacao> getAllTransacoes() { return transacaoService.findAll(); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transacao> getTransacaoById(@PathVariable Long id) { Optional<Transacao> transacao = transacaoService.findById(id); return transacao.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Transacao createTransacao(@RequestBody Transacao transacao) { return transacaoService.criarTransacao(transacao);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTransacao(@PathVariable Long id) { try { transacaoService.cancelarTransacao(id);
+        return ResponseEntity.noContent().build();
+    } catch (Exception e) {
+        return ResponseEntity.notFound().build();
+    }
+    }
+
 }
