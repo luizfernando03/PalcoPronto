6a74bfe25afb69a6d5dab4e4c7ff84a303ae9ea2
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-17 10:55:28 -0300

    Criando interface EventoService e classe EventoServiceImpl

---
+++ src/main/java/br/com/javeirosavante/palcopronto/service/EventoServiceImpl.java
@@ -0,0 +1,7 @@
+package br.com.javeirosavante.palcopronto.service;
+
+import br.com.javeirosavante.palcopronto.repository.EventoRepository;
+
+public class EventoServiceImpl implements EventoService {
+
+}
