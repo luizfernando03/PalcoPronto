fe6074a8956ec50fd36d00f3ae867bcde96b5bc9
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-17 15:55:10 -0300

    Subindo dados da classe IngressoController

--- src/main/java/br/com/javeirosavante/palcopronto/controller/IngressoController.java
+++ src/main/java/br/com/javeirosavante/palcopronto/controller/IngressoController.java
@@ -1,4 +1,27 @@
 package br.com.javeirosavante.palcopronto.controller;
 
+import br.com.javeirosavante.palcopronto.model.Ingresso;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/ingresso")
 public class IngressoController {
+
+    @Autowired
+    private IngressoService ingressoService;
+
+    @GetMapping
+    public List<Ingresso> getAllIngressos() { return ingressoService.findAll(); }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Ingresso> getIngressoById(@PathVariable Long id) { Optional<Ingresso> ingresso = ingressoService.findById(id);
+
 }
