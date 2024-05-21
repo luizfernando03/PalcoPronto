cc3beccf32119cfdc9006f1b1d727084fe94cc1e
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-21 09:52:39 -0300

    Criando a classe EspacoNaoExistenteException no validator

---
+++ src/main/java/br/com/javeirosavante/palcopronto/validator/EspacoNaoExistenteException.java
@@ -0,0 +1,8 @@
+package br.com.javeirosavante.palcopronto.validator;
+
+public class EspacoNaoExistenteException extends RuntimeException{
+
+    public EspacoNaoExistenteException(String msg) {
+        super(msg);
+    }
+}
