9066a2e7475b83558bd9bbf927bf08bd6cbc1709
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-21 09:40:08 -0300

    Criando a classe EspacoServiceImpl

---
+++ src/main/java/br/com/javeirosavante/palcopronto/service/EspacoServiceImpl.java
@@ -0,0 +1,12 @@
+package br.com.javeirosavante.palcopronto.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EspacoServiceImpl {
+
+
+
+}
