d14144479453c3da9676f0b130aeb20f4329c0aa
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-21 10:01:40 -0300

    Criando a classe EspacoExistenteException no validator

---
+++ src/main/java/br/com/javeirosavante/palcopronto/validator/IngressoJaExistenteException.java
@@ -0,0 +1,9 @@
+package br.com.javeirosavante.palcopronto.validator;
+
+public class IngressoJaExistenteException extends RuntimeException{
+
+    public IngressoJaExistenteException(String msg) {
+        super(msg);
+    }
+    
+}
