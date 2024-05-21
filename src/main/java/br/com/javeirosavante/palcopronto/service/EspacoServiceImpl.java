e1d5091c7d38df69b219739231cf6f6cc492750e
Luiz fernando <luizfernandodemoura.2013@gmail.com>
2024-05-21 09:45:41 -0300

    Subindo dados da classe EspacoServiceImpl

--- src/main/java/br/com/javeirosavante/palcopronto/service/EspacoServiceImpl.java
+++ src/main/java/br/com/javeirosavante/palcopronto/service/EspacoServiceImpl.java
@@ -1,12 +1,73 @@
 package br.com.javeirosavante.palcopronto.service;
 
+import br.com.javeirosavante.palcopronto.mapper.EspacoMapper;
+import br.com.javeirosavante.palcopronto.model.Espaco;
+import br.com.javeirosavante.palcopronto.repository.EspacoRepository;
+import br.com.javeirosavante.palcopronto.validator.EspacoExistenteException;
+import br.com.javeirosavante.palcopronto.validator.EspacoNaoExistenteException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class EspacoServiceImpl {
 
+    @Autowired
+    private EspacoRepository repository;
 
+    @Override
+    public List<Espaco> findAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Optional<Espaco> findById(Long idEspaco) {
+        return repository.findById(idEspaco);
+    }
+
+
+
+//    @Override
+//    public Optional<Espaco> findEventoByNameEventoContainingIgnoreCase(String nomeEspaco) {
+//        return Optional.empty();
+//    }
+
+    @Override
+    public Espaco criarEspaco(Espaco espacoDto) {
+        if(repository.findById(espacoDto.getIdEspaco()).isPresent())
+            throw new EspacoExistenteException("Espaço já cadastrado");
+
+        return repository.save(espacoDto);
+    }
+
+    @Override
+    public Optional<Espaco> atualizarEspaco(Long idEspaco, Espaco espacoAtualizado) {
+        final Optional<Espaco> espacoOptional = repository.findById(idEspaco);
+        if (espacoOptional.isPresent()) {
+            final Espaco espacoEncontrado = espacoOptional.get();
+
+            EspacoMapper.copy(espacoEncontrado, espacoAtualizado);
+
+            repository.save(espacoAtualizado);
+
+            return Optional.of(espacoAtualizado);
+        }
+
+        return Optional.empty();
+    }
+
+    @Override
+    public void deletarEspaco(Long idEspaco) {
+        final Optional<Espaco> espacoOptional = repository.findById(idEspaco);
+
+        if(espacoOptional.isPresent())
+            repository.delete(espacoOptional.get());
+
+        throw  new EspacoNaoExistenteException("O espaço não existe!");
+    }
 
 }
